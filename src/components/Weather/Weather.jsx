cbe083e81a86a6a48767d6aca01f75fe87ab7e2e
Liana Petrova <lianapetrova00@gmail.com>
2023-06-21 14:33:19 +0100

    unsaved changes

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import "./Weather.scss"
+import Button from '../Button/Button';
+
 
 const Weather = () => {
     const [weather, setWeather] = useState();
@@ -11,21 +13,23 @@ const Weather = () => {
     }
 
     const getWeather = async()=> {
-        let url = `${api.base}/current.json?key=${api.key}&q=............`;
+        let url = `${api.base}/current.json?key=${api.key}&q=q=51.473929,-0.020256`;
         const response = await fetch(url);
         const data = await response.json();
-        setWeather(data)
+        setWeather(data.current.humidity)
+        console.log(data.current.humidity)
     }
 
     const getForecast = async()=> {
-        let url = `${api.base}/forecast.json?key=${api.key}&q=............`;
+        let url = `${api.base}/forecast.json?key=${api.key}&q=51.473929,-0.020256&days=7`;
         const response = await fetch(url);
-        const data = await response.json();
-        setForecast(data)
+        const data2 = await response.json();
+        setForecast(data2)
+        console.log(data2)
     }
   return (
     <>
-    <div>Weather</div>
+    <p>Weather</p>
     <Button buttonText={"Get Current Weather"} onClick={getWeather}/>
     <p>Current Weather: {weather}</p>
     <Button buttonText={"Get Forecast"} onClick={getForecast}/>
